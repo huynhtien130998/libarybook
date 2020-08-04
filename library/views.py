a01c72855cf2764bd171407a7f5ac80a06c33734
huynhtien130998 <68526456+huynhtien130998@users.noreply.github.com>
2020-08-04 14:07:47 +0700

    update
    update cateview.html

--- library/views.py
+++ library/views.py
@@ -30,9 +30,20 @@ def index(request):
 def cate_view(request, category_id):
     cate_view= Category.objects.get(pk = category_id)
     cateitem = Category.objects.all()
+    bookitem2 = Book.objects.all()
+
+    paginator = Paginator(bookitem2, 3)
+    page_Number = request.GET.get('page', 1)
+    try:
+        bookpage2 = paginator.page(page_Number)
+    except PageNotAnInteger:
+        bookpage2 = paginator.page(1)
+    except EmptyPage:
+        bookpage2 = paginator.page(paginator.num_pages)
 
     context1={
         'cate':cate_view,
         'catename': cateitem,
+        'paging2': bookpage2,
     }
     return render(request, "library/cateview.html", context1)
